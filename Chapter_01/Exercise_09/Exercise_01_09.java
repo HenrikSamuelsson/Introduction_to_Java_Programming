209ec381e7955ff0e0f83dae4e70f5aee567ed2d
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-08 15:37:24 +0100

    Initial commit of exercise 1.9

---
+++ Chapter_01/Exercise_09/Exercise_01_09.java
@@ -0,0 +1,17 @@
+/**
+ * Exercise 1.9 from the book Introduction to Java Programming (10th).
+ * <p>
+ * Write a program that displays the area and perimeter of a rectangle with
+ * the width of 4.5 and height of 7.9.
+ *  
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+public class Exercise_01_09 {
+
+	public static void main(String[] args) {
+		System.out.println( 4.5 + 7.9 + 4.5 + 7.9 );	// perimeter
+		System.out.println( 4.5 * 7.9 );	// area
+	}
+
+}
