023a34b079e68f993680210e2bc92ffb2ceec72b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-08 11:20:45 +0100

    Initial committ

---
+++ Chapter_01/Exercise_02/Exercise_01_02.java
@@ -0,0 +1,19 @@
+/**
+ * Exercise 1.2 from the book Introduction to Java Programming (10th).
+ * <p>
+ * Write a program that displays "Welcome to Java" five times.
+ * <p>
+ * I choose to use a loop in this exercise, because it is the natural choice,
+ * even though this concept have not yet been introduced in the text.
+ * 
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+public class Exercise_01_02 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; ++i) {
+			System.out.println("Welcome to Java");
+		}
+	}
+
+}
