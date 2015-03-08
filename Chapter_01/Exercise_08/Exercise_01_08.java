8c188d3f3a40c6116395c44c32792158114807ed
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-08 15:25:52 +0100

    Intial commit of exercise 1.8

---
+++ Chapter_01/Exercise_08/Exercise_01_08.java
@@ -0,0 +1,17 @@
+/**
+ * Exercise 1.8 from the book Introduction to Java Programming (10th).
+ * <p>
+ * Write a program that displays the area and perimeter of a circle that has a
+ * radius of 5.5.
+ * 
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+public class Exercise_01_08 {
+
+	public static void main(String[] args) {
+		System.out.println( 2.0 * 5.5 * 3.14 );
+		System.out.println( 5.5 * 5.5 * 3.14 );
+	}
+
+}
