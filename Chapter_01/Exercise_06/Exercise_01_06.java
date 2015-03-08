3ee8d71ef3fc01af063e2952d9f046f3f9901f13
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-08 14:58:14 +0100

    Inital committ of exercis 1.6.

---
+++ Chapter_01/Exercise_06/Exercise_01_06.java
@@ -0,0 +1,13 @@
+/**
+ * Exercise 1.6 from the book Introduction to Java Programming (10th).
+ *
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+public class Exercise_01_06 {
+
+	public static void main(String[] args) {
+		System.out.println( 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 );
+	}
+
+}
