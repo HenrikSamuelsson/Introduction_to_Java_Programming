a1e377b16ca1e23e5572226fd4037ef1d0f8deea
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-08 14:38:49 +0100

    Intial commit of exercise 1.5

---
+++ Chapter_01/Exercise_05/Exercise_01_05.java
@@ -0,0 +1,12 @@
+/**
+ * Exercise 1.5 from the book Introduction to Java Programming (10th).
+ * 
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+public class Exercise_01_05 {
+
+	public static void main(String[] args) {
+		System.out.println( ( 9.5 * 4.5 - 2.5 * 3 ) / ( 45.5 - 3.5 ) );
+	}
+
+}
