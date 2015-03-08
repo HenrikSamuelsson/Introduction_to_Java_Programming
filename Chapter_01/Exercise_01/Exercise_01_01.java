11f3a38b2701efee7712f027ba61a086f6b6ec28
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-08 10:51:37 +0100

    Adding exercise 1.1

---
+++ Chapter_01/Exercise_01/Exercise_01_01.java
@@ -0,0 +1,17 @@
+/**
+ * Exercise 1.1 from the book Introduction to Java Programming.
+ * <p>
+ * Write a program that displays "Welcome to Java", 
+ * "Welcome to Computer Science", and "Programming is fun".
+ * 
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+public class Exercise_01_01 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+	}
+
+}
