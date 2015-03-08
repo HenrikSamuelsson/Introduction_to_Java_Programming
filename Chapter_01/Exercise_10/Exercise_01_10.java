afffaff554dc8b00a9a6bc49fc7aaa29d5d6d6a8
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-08 18:37:00 +0100

    Edit of exercise 1.10

--- Chapter_01/Exercise_10/Exercise_01_10.java
+++ Chapter_01/Exercise_10/Exercise_01_10.java
@@ -9,6 +9,7 @@ public class Exercise_01_10 {
 		// 14 / 1.6 to convert km to miles
 		// 45 minutes and 30 seconds equals 45.5 minutes
 		// 45.5 / 60 to convert minutes to hours
+		// finally divide miles / hours to get the average speed
 		System.out.println( ( 14.0 / 1.6 ) / ( 45.5 / 60 ) );
 	}
 
