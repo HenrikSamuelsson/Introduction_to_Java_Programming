9a1352fa64524d282fde8d13a0be7cf44823c306
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-08 19:41:44 +0100

    Inital commit of exercise 12

---
+++ Chapter_01/Exercise_12/Exercise_01_12.java
@@ -0,0 +1,14 @@
+/**
+ * Exercise 1.12 from the book Introduction to Java Programming (10th).
+ *  
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+public class Exercise_01_12 {
+	
+	public static void main(String[] args) {
+		System.out.println( ( 24.0 / 1.6 ) 					// km
+				/ ( 1 + 40.0 / 60.0 + 35.0 / 3600.0 ) );	// hours
+	}
+
+}
