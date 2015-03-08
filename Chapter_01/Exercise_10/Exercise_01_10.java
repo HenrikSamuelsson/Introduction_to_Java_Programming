fe0e5489e71bd2cda9f743875e9f36b19d2e2c54
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-08 18:34:30 +0100

    Inital commit of exercise 1.10

---
+++ Chapter_01/Exercise_10/Exercise_01_10.java
@@ -0,0 +1,15 @@
+/**
+ * Exercise 1.10 from the book Introduction to Java Programming (10th).
+ *  
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+public class Exercise_01_10 {
+	
+	public static void main(String[] args) {
+		// 14 / 1.6 to convert km to miles
+		// 45 minutes and 30 seconds equals 45.5 minutes
+		// 45.5 / 60 to convert minutes to hours
+		System.out.println( ( 14.0 / 1.6 ) / ( 45.5 / 60 ) );
+	}
+
+}
