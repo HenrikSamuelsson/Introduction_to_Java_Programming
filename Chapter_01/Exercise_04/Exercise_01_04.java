cee8f69951bf16e906c8477ac614a18562e579d8
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-08 14:28:12 +0100

    Inital commit

---
+++ Chapter_01/Exercise_04/Exercise_01_04.java
@@ -0,0 +1,19 @@
+/**
+ * Exercise 1.4 from the book Introduction to Java Programming (10th).
+ * <p>
+ * A quick and dirty solution to this exercise.
+ * 
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+public class Exercise_01_04 {
+
+	public static void main(String[] args) {
+		System.out.println("a\ta^2\ta^3");
+		System.out.println("1\t1\t1");
+		System.out.println("2\t4\t8");
+		System.out.println("3\t9\t27");
+		System.out.println("4\t16\t64");
+	}
+
+}
