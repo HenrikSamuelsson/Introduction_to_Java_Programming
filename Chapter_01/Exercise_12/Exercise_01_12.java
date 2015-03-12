5d00d3377193c509abf1224666048eff02cf4d66
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-03-12 15:46:03 +0100

    Update Exercise_01_12.java

--- Chapter_01/Exercise_12/Exercise_01_12.java
+++ Chapter_01/Exercise_12/Exercise_01_12.java
@@ -7,7 +7,7 @@
 public class Exercise_01_12 {
 	
 	public static void main(String[] args) {
-		System.out.println( ( 24.0 / 1.6 )	// miles to km
+		System.out.println( ( 24.0 / 1.6 )	// distance in km
 				/ ( 1 + 40.0 / 60.0 + 35.0 / 3600.0 ) );    // time in hours
 	}
 
