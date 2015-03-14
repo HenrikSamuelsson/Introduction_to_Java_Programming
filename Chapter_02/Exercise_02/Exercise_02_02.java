b137d8266afb0c3657c24ae48ca86f7450c8150b
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-14 19:13:10 +0100

    Adding Exercise 2.1

---
+++ Chapter_02/Exercise_02/Exercise_02_02.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Exercise 2.1 from the book Introduction to Java Programming (10th).
+ * <p>
+ *
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+public class Exercise_02_02 {
+
+	public static void main(String[] args) {
+		double radius;
+		double length;
+		double area;
+		double volume;
+		
+		// get input from user
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the radius and length of a cylinder: ");
+		radius = input.nextDouble();
+		length = input.nextDouble();
+		
+		// calculate area and volume
+		area = radius * radius * Math.PI;
+		volume = area * length;
+		
+		// present result
+		System.out.println("The area is " + area);
+		System.out.println("The volume is " + volume);
+	}
+
+}
