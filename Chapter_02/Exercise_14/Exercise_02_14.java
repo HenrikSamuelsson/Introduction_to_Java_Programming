24a3f0714daad3ed568c289b32d120e671545079
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-16 22:16:43 +0100

    Inital commit of exercise 2.14.

---
+++ Chapter_02/Exercise_14/Exercise_02_14.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Exercise 2.14 from the book Introduction to Java Programming (10th).
+ *
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+public class Exercise_02_14 {
+
+	public static void main(String[] args) {
+		double height;
+		double weight;
+		double bmi;
+		
+		// get weight and height from the user
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter weight in pounds: ");
+		weight = input.nextDouble();
+		System.out.print("Enter height in inches: ");
+		height = input.nextDouble();
+		
+		// convert weight to kilograms
+		weight = weight * 0.45359237;
+		// convert height to meters
+		height = height * 0.0254;
+		// calculate BMI
+		bmi = weight / (height * height);
+		
+		// display the calculated BMI to the user
+		System.out.print("BMI is " + bmi);
+		
+	}
+
+}
