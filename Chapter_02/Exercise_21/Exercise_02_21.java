7ad8ce9c0d5fe2fa4be89481a60977a355a0c992
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-18 00:18:50 +0100

    Initial commit of exercise 2.21

---
+++ Chapter_02/Exercise_21/Exercise_02_21.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Exercise 2.21 from the book Introduction to Java Programming (10th).
+ *
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+public class Exercise_02_21 {
+
+	public static void main(String[] args) {
+		double futureValue, investment, interest, years;
+		
+		// get input from user
+		Scanner input = new Scanner(System.in);
+		System.out.print ("Enter investment amount: ");
+		investment = input.nextDouble();
+		System.out.print ("Enter annual interest rate in percentage: ");
+		interest = input.nextDouble();
+		System.out.print ("Enter number of years: ");
+		years = input.nextDouble();
+		
+		// calculate the future value of the investment
+		futureValue = investment * Math.pow(1 + interest / 1200, years * 12);
+		
+		// display the result of the calculation
+		System.out.println("Accumulated value is $" + futureValue);
+	}
+
+}
