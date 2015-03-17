fe4901d49ea7caed201441d207ba802e3d185f2d
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-17 23:17:12 +0100

    Initial commit of exercise 2.19

---
+++ Chapter_02/Exercise_19/Exercise_02_19.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * Exercise 2.19 from the book Introduction to Java Programming (10th).
+ * 
+ * @note I did not understand the suggested formula in the book so I searched
+ *       for a formula online and found this site: 
+ *       	http://www.mathopenref.com/coordtrianglearea.html
+ *       There was another formula that I ended up using.
+ *       
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+public class Exercise_02_19 {
+
+	public static void main(String[] args) {
+		double x1, x2, x3, y1, y2, y3;
+		double area;
+		
+		// query the user for data about the three points
+		Scanner input = new Scanner(System.in);
+		System.out.print ("Enter three points for a triangle: ");
+		x1 = input.nextDouble();
+		y1 = input.nextDouble();
+		x2 = input.nextDouble();
+		y2 = input.nextDouble();
+		x3 = input.nextDouble();
+		y3 = input.nextDouble();
+		
+		// calculate the area of the triangle
+		area = Math.abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2);
+		
+		// display the result
+		System.out.println(area);
+	}
+
+}
