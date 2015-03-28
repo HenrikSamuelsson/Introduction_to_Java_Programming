03264410c3f57fe83223d7d8c57277d414893304
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-03-28 21:20:53 +0100

    More stupid solution but simpler to follow for absolute beginners.

--- Chapter_01/Exercise_02/Exercise_01_02.java
+++ Chapter_01/Exercise_02/Exercise_01_02.java
@@ -2,18 +2,17 @@
  * Exercise 1.2 from the book Introduction to Java Programming (10th).
  * <p>
  * Write a program that displays "Welcome to Java" five times.
- * <p>
- * I choose to use a loop in this exercise, because it is the natural choice,
- * even though this concept have not yet been introduced in the text.
- * 
+ *
  * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
  */
 public class Exercise_01_02 {
 
 	public static void main(String[] args) {
-		for(int i = 0; i < 5; ++i) {
-			System.out.println("Welcome to Java");
-		}
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
 	}
 
 }
