6811278f0ee7db194a4f81c78a0aedeaaa261a9f
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-04-08 23:34:51 +0200

    Edit of exercise 6.1.

--- Chapter_06/Exercise_01/Exercise_06_01.java
+++ Chapter_06/Exercise_01/Exercise_06_01.java
@@ -1,16 +1,21 @@
+/**
+ * Exercise 6.1 from the book Introduction to Java Programming (10th).
+ *
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
 
 public class Exercise_06_01 {
 
 	public static void main(String[] args) {
-		for(int nr = 1; nr <= 100; nr++ ){
+		for (int nr = 1; nr <= 100; nr++) {
 			System.out.printf("%5d ", getPentagonalNumber(nr));
-			if(nr % 10 == 0)
+			if (nr % 10 == 0)
 				System.out.println("");
 		}
 	}
-	
+
 	public static int getPentagonalNumber(int n) {
-		return n*(3 * n - 1) / 2;
+		return n * (3 * n - 1) / 2;
 	}
 
 }
