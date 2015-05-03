e242b0fc28fc8426cfd2c44890c83977a098349a
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-05-03 19:52:28 +0200

    Edits of files for exercise 9.6.

---
+++ Chapter_09/Exercise_06/Exercise_09_06.java
@@ -0,0 +1,19 @@
+/**
+ * Exercise 9.6 from the book Introduction to Java Programming (10th).
+ *
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+
+
+public class Exercise_09_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class StopWatch {
+	
+}
