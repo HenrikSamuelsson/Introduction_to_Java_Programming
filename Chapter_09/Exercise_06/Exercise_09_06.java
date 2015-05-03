ed5a00fd46d693f0b0cb53b98485afdadd4b8cc1
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-05-03 20:36:50 +0200

    Solution for exercise 9.6.

--- Chapter_09/Exercise_06/Exercise_09_06.java
+++ Chapter_09/Exercise_06/Exercise_09_06.java
@@ -4,16 +4,54 @@
  * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
  */
 
+import java.util.Random;
 
 public class Exercise_09_06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final int DATA_AMOUNT = 100_000;
+		int[] data = new int[DATA_AMOUNT];
+		StopWatch stopWatch = new StopWatch();
 
+		// generate some random data for our data array
+		Random rand = new Random();
+		for (int i = 0; i < DATA_AMOUNT; i++) {
+			data[i] = rand.nextInt();
+		}
+		
+		// sort the data and measure the time that the sorting takes
+		stopWatch.start();
+		java.util.Arrays.sort(data);
+		stopWatch.stop();	
+		
+		// display the result
+		System.out.println(stopWatch.getElapsedTime());
 	}
 
 }
 
 class StopWatch {
+	private long startTime;
+	private long endTime;
+
+	/** Constructs a stop watch with start time set to the current time. */
+	StopWatch() {
+		startTime = System.currentTimeMillis();
+	}
+
+	/** Sets the start time. */
+	void start() {
+		startTime = System.currentTimeMillis();
+	}
+
+	/** Sets the end time. */
+	void stop() {
+		endTime = System.currentTimeMillis();
+	}
+	
+	/** Calculates the elapsed time between start and stop. */
+	long getElapsedTime() {
+		return endTime - startTime;
+	}
 	
 }
