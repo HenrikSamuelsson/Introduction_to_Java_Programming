6ff3cd0fe1bbf9e16a3d352513c82c9f10d33ade
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-05-24 21:09:35 +0200

    Edit of exercise 5.3.

--- Chapter_05/Exercise_03/Exercise_05_03.java
+++ Chapter_05/Exercise_03/Exercise_05_03.java
@@ -9,9 +9,16 @@
 public class Exercise_05_03 {
 
 	public static void main(String[] args) {
-		System.out.println("Kilograms\tPounds");
+		
+		// print the heading of the table
+		String kilograms = "Kilograms";
+		String pounds = "Pounds";
+		System.out.printf("%-10s %10s %n", kilograms, pounds);
+		
+		// print the data in the table by the use of a loop
+		// will left-justify the kilograms and right-justify the pounds
 		for (int i = 1; i < 200; i = i + 2) {
-			System.out.println(i + String.format("\t\t%.1f", i * 2.2));
+			System.out.printf("%-10.0f %10.1f %n", (double) i, i * 2.2);
 		}
 	}
 
