de6d4d24e2b9a584fba00559a572e568046d8c30
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2015-05-26 17:41:35 +0200

    Solution for exercise 7.1.

---
+++ Chapter_07/Exercise_02/Exercise_07_02.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Exercise 7.2 from the book Introduction to Java Programming (10th).
+ * <p>
+ * Reverse the numbers entered.
+ * 
+ * @author Henrik Samuelsson, henrik.samuelsson(at)gmail.com
+ */
+public class Exercise_07_02 {
+
+	public static void main(String[] args) {
+		final int NUMBER_COUNT = 10;
+		int[] numbers = new int[NUMBER_COUNT];
+		
+		Scanner input = new Scanner(System.in);
+		
+		// get the numbers from the user
+		for (int i = 0; i < NUMBER_COUNT; i++) {
+			numbers[i] = input.nextInt();
+		}
+		
+		// print all the numbers in reverse
+		for (int i = NUMBER_COUNT; i > 0; i--) {
+			System.out.print(numbers[i - 1] + " ");
+		}
+
+	}
+
+}
