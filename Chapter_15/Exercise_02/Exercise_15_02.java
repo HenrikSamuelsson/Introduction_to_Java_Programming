06b6ab7bdc5dfb3be37dc7d3bf418a52b8874faf
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-08-30 19:13:06 +0200

    Adds exercise 15.2.

---
+++ Chapter_15/Exercise_02/Exercise_15_02.java
@@ -0,0 +1,55 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+/**
+ * Exercise 15.02 from the book Introduction to Java Programming (10th).
+ * <p>
+ * Rotate a rectangle
+ */
+
+public class Exercise_15_02 extends Application {
+	private double angle = 0;
+
+	@Override
+	// Override the start method in the Application class
+	public void start(Stage primaryStage) {
+		StackPane stackPane = new StackPane();
+		Rectangle rectangle = new Rectangle(30, 30, 30, 60);
+		rectangle.setFill(Color.WHITE);
+		rectangle.setStroke(Color.BLACK);
+		stackPane.getChildren().add(rectangle);
+
+		// Create a button
+		Button btRotate = new Button("Rotate");
+		btRotate.setOnAction(e -> {
+			angle += 15;
+			rectangle.setRotate(angle);
+		});
+
+		BorderPane pane = new BorderPane();
+		pane.setCenter(stackPane);
+		pane.setBottom(btRotate);
+		BorderPane.setAlignment(btRotate, Pos.TOP_CENTER);
+
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(pane, 210, 150);
+		primaryStage.setTitle("Exercise_15_02"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage
+		primaryStage.show(); // Display the stage
+	}
+
+	/**
+	 * The main method is only needed for the IDE with limited JavaFX support.
+	 * Not needed for running from the command line.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
